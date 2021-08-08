33de97bfc5cf26001c6d614e569700fb2ee5c77d
Anna Storozhuk <annastorozhuk17@gmail.com>
2021-08-08 15:48:37 +0300

    header+FOOTER
    cursor+css

--- src/js/header.js
+++ src/js/header.js
@@ -1,33 +1,36 @@
-const Header = {
+const Head = {
     Home: 'header-1',
     Lib: 'header-2',
     hidden: 'is-hidden',
+    
 }
-const hidden= document.querySelector('is-hidden')
+
 const header = document.querySelector('header')
+
 
 
 const libNav = document.querySelector('.js-lib-menu');
 const homeNav = document.querySelector('.js-home-menu');
 const formNav = document.querySelector('.search-form');
-const libList = document.querySelector('.library-list')
-  
+const libList = document.querySelector('.library-list');
+
+
 libNav.addEventListener('click', libPageOn);
 homeNav.addEventListener('click', homePageOn);
 
 
 function libPageOn() {
 
-    header.classList.add(Header.Lib);
-    header.classList.remove(Header.Home);
-    formNav.classList.add(Header.hidden);
-    libList.classList.remove(Header.hidden);
+    header.classList.add(Head.Lib);
+    header.classList.remove(Head.Home);
+    formNav.classList.add(Head.hidden);
+    libList.classList.remove(Head.hidden);
 }
 
 function homePageOn () {
-    
-    header.classList.remove(Header.Lib);
-    header.classList.add(Header.Home);
-    formNav.classList.remove(Header.hidden);
-    libList.classList.add(Header.hidden);
+   
+    header.classList.remove(Head.Lib);
+    header.classList.add(Head.Home);
+    formNav.classList.remove(Head.hidden);
+    libList.classList.add(Head.hidden);
 }
