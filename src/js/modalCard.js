2802513547fea45d18950b3a09d5d0e7df2e3c88
Olena Avdiunina <elena.avdiunina@gmail.com>
2021-08-10 13:30:06 +0300

    add backdrop

--- src/js/modalCard.js
+++ src/js/modalCard.js
@@ -1,89 +1,14 @@
-import modalCard from '../hbs/sample-2.hbs';
-import basiclightbox from 'basiclightbox';
-import API_KEY from './key';
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-
-const cardFilm = document.querySelector('.js-container');
-cardFilm.addEventListener('click', openModal);
-
-function fetchMovie(movie_id) {
-    const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}`;
-    return fetch(url)
-      .then(response => response.json());
-}
-
-function renderMovieModal(data) {
-
-    const modalMarkup = modalCard(data);
-      modalBox.innerHTML = modalMarkup;
-      modalBox.classList.add('is-open');
-      document.body.style.overflow = 'hidden';
-  
-      const modal = document.querySelector('.modal');
-      const modalCloseBtn = document.querySelector('[data-action="close-modal"]');
-  
-      modal.addEventListener('click', modalClose);
-      modalCloseBtn.addEventListener('click', modalClose);
-      window.addEventListener('keydown', modalCloseByEsc);
-
-    // Закрытие модалки
-      function modalClose() {
-        modal.classList.remove('is-open');
-        document.body.style.overflow = '';
-        window.removeEventListener('keydown', modalCloseByEsc);
-      }
-
-      // Закрытие модалки по Escape
-      function modalCloseByEsc(event) {
-      if (event.code === 'Escape') {
-      modalClose();
-      }
-}
-  
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
   }
-
- 
-
-
-
-// function openModal(e) {
-//   e.preventDefault();
-  
-//   fetchMovie(e.target.dataset.id)
-//       .then(data => {
-//         if (e.target.nodeName !== 'IMG')
-//         return;
-
-//         const markup = modalFilmCard(data);
-//         const modal = basicLightbox.create(markup);
-
-//         modal.show();
-
-//         const closeBtn = document.querySelector('.modal-close-btn');
-//         closeBtn.addEventListener('click', closeModal);
-  
-//         window.addEventListener('keydown', closeModalHandler);
-  
-//         function closeModalHandler(e) {
-//           if (e.code === 'Escape') {
-//             modal.close();
-//             window.removeEventListener('keydown', closeModalHandler);
-//           }
-//         }
-  
-//         function closeModal(e) {
-//           modal.close();
-//           window.removeEventListener('keydown', closeModalHandler);
-//         }
-  
-      
-//       })
-  
-//   }
-
-
-
-
-
-  
+})();
