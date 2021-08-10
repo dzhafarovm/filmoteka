1c18adb269642b56ac78b0283811cea3ad0ebea3
OlenaAwd <elena.avdiunina@gmail.com>
2021-08-11 00:36:34 +0300

    add open/close fn

--- src/js/modalCard.js
+++ src/js/modalCard.js
@@ -1,5 +1,7 @@
 import { refs } from './refs.js';
 import modalTpl from '../hbs/sample-2.hbs';
+import axios from "axios";
+import KEY_API from './key';
 
 // Поиск ссылки на крточку и назначение слушателя
 export function openModalListener() {
@@ -14,3 +16,47 @@ function toggleModal(e) {
   //   renderMovieModal();
 }
 
+  // Фетч фильма по  ID
+  function fetchMovieById(movie_id) {
+    const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${KEY_API}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => ({
+        ...data,
+        year: createYear(data),
+        popularity: data.popularity.toFixed(1),
+
+      }));
+}
+ //Открытие модалки
+  function openModal(e) {
+  e.preventDefault();
+
+  fetchMovieById(e.target.dataset.id)
+    .then(data => {
+      if (e.target.nodeName !== 'IMG') return;
+      const markup = modalTpl(data);
+      const modal = basicLightbox.create(markup);
+
+      modal.show();
+
+      const closeBtn = document.querySelector('.modal-close-btn');
+      closeBtn.addEventListener('click', closeModal);
+
+      window.addEventListener('keydown', closeModalHandler);
+
+      function closeModalHandler(e) {
+        if (e.code === 'Escape') {
+          modal.close();
+          window.removeEventListener('keydown', closeModalHandler);
+        }
+      }
+
+      function closeModal(e) {
+        modal.close();
+        window.removeEventListener('keydown', closeModalHandler);
+      }
+
+  })
+};
+    
