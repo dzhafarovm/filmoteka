7b4c03ef7da023ff28a4191842f8a5a78a852d62
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-08-11 18:28:55 +0300

    вывел модалку

--- src/js/modalCard.js
+++ src/js/modalCard.js
@@ -1,69 +1,68 @@
 import { refs } from './refs.js';
 import modalTpl from '../hbs/sample-2.hbs';
 import axios from 'axios';
-import KEY_API from './key';
+import { KEY_API } from './key';
 
-// Поиск ссылки на крточку и назначение слушателя
+//////////////////////////////////////////////////////////////////////////////
+////// Окрытие модалки - нажатие на карточку и назначение слушателя
 export function openModalListener() {
   refs.filmsContainer.addEventListener('click', onPaletteContainerClick);
 }
+
+// слушатель
 function onPaletteContainerClick(evt) {
   if (!evt.target.classList.contains('film-image')) {
     return;
   }
   refs.backdrop.classList.remove('is-hidden');
-  console.log(evt.target);
+  dataIdSearch();
 }
 
-// export function openModalListener() {
-//   const cardLink = document.querySelector('.card-link');
-//   cardLink.addEventListener('click', toggleModal);
-// }
-
-// Поведение по нажатю (слушатель)
-// function toggleModal(e) {
-//   e.preventDefault();
-//   refs.backdrop.classList.remove('is-hidden');
-//   //   renderMovieModal();
-// }
-
-// Фетч фильма по  ID
-function fetchMovieById(movie_id) {
-  const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${KEY_API}`;
-  return fetch(url)
-    .then(response => response.json())
-    .then(data => ({
-      ...data,
-      year: createYear(data),
-      popularity: data.popularity.toFixed(1),
-    }));
+//////////////////////////////////////////////////////////////////////////////
+//////// Фетч фильма по  ID
+function fetchId() {
+  return axios
+    .get(`https://api.themoviedb.org/3/movie/${520763}?api_key=${KEY_API}`)
+    .then(response => {
+      // console.log(response.data);
+      return response.data;
+    });
 }
-//Открытие модалки
-function openModal(e) {
-  e.preventDefault();
 
-  fetchMovieById(e.target.dataset.id).then(data => {
-    if (e.target.nodeName !== 'IMG') return;
-    const markup = modalTpl(data);
-    const modal = basicLightbox.create(markup);
-
-    modal.show();
-
-    const closeBtn = document.querySelector('.modal-close-btn');
-    closeBtn.addEventListener('click', closeModal);
-
-    window.addEventListener('keydown', closeModalHandler);
-
-    function closeModalHandler(e) {
-      if (e.code === 'Escape') {
-        modal.close();
-        window.removeEventListener('keydown', closeModalHandler);
-      }
-    }
-
-    function closeModal(e) {
-      modal.close();
-      window.removeEventListener('keydown', closeModalHandler);
-    }
-  });
+// Данные - вызов
+function dataIdSearch() {
+  fetchId()
+    .then(renderMovieModal)
+    .catch(error => {
+      console.log(error);
+    });
 }
+//////////////////////////////////////////////////////////////////////////////
+// рендеринг шаблона
+function renderMovieModal(data) {
+  console.log(data);
+  const collectionIdFilm = data;
+  //   console.log(collectionIdFilm);
+
+  const modalMarkup = modalTpl(data);
+  console.log(modalMarkup);
+  refs.backdrop.innerHTML = modalMarkup;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Закрытие модалки
+function modalClose() {
+  refs.backdrop.classList.add('is-hidden');
+}
+
+// Закрытие модалки по Escape
+function modalCloseByEsc(event) {
+  if (event.code === 'Escape') {
+    modalClose();
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////
+//
+const modalCloseBtn = document.querySelector('.backdrop');
+modalCloseBtn.addEventListener('click', modalClose);
