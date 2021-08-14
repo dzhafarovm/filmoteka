23d52d1877c527d05b313e9aa9afcadcbaee5ef1
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-08-14 14:32:30 +0300

    1

--- src/js/library.js
+++ src/js/library.js
@@ -1,12 +1,23 @@
 import { refs } from './refs.js';
 import collectionLibraryCardTpl from '../hbs/sample-1.hbs';
+import { watched, queue } from './modal-btn';
 
+//Поиск ссылок по карточке
+function searchLinksLib() {
+  return {
+    listItemLib: document.querySelector('.gallery-item'),
+  };
+}
+
+// Слушатели
 refs.btnLib.addEventListener('click', opensLibraryddd);
 
+//////////////////////////////////////////////
 // По слущателю - очитска страници
 function opensLibraryddd() {
   refs.filmsContainer.innerHTML = '';
   allStorage();
+  listenerBtnLib();
 }
 
 // Вызов из хранилищя, переборы и рендеринг
@@ -17,11 +28,63 @@ function allStorage() {
   while (i--) {
     archive[keys[i]] = JSON.parse(localStorage.getItem(keys[i]));
   }
-
+  //   console.log(archive);
   archive.forEach(function (item) {
     //  console.log(item);
     //  console.log(item[0].librarySection);
+    //   librarySection;
+    //   console.log(item[0].genres);
+    //  console.log(item[0].genres.replace(/\n/g, ''));
+    //  item[0].genres.replace(/\n/g, '');
+    //  console.log(item[0].librarySection);
 
     refs.filmsContainer.insertAdjacentHTML('beforeend', collectionLibraryCardTpl(item));
+  });
+}
+
+///////////////////////////////////////////////
+// filtersWatched();
+//////Кнопки Библиотеки
+//CСлушатели
+function listenerBtnLib() {
+  refs.btnWatched.addEventListener('click', filtersWatched);
+  refs.btnQueue.addEventListener('click', filtersQueue);
+}
+
+// Кнопка - Watched
+function filtersWatched() {
+  refs.filmsContainer.innerHTML = '';
+  //   console.log('Wwww');
+  let archive = [],
+    keys = Object.keys(localStorage),
+    i = keys.length;
+  while (i--) {
+    archive[keys[i]] = JSON.parse(localStorage.getItem(keys[i]));
+  }
+
+  archive.forEach(function (item) {
+    const dataWatched = item[0].librarySection;
+    if (dataWatched === watched) {
+      refs.filmsContainer.insertAdjacentHTML('beforeend', collectionLibraryCardTpl(item));
+    }
+  });
+}
+
+// Кнопка - Queue
+function filtersQueue() {
+  //   console.log('QQQQ');
+  refs.filmsContainer.innerHTML = '';
+  let archive = [],
+    keys = Object.keys(localStorage),
+    i = keys.length;
+  while (i--) {
+    archive[keys[i]] = JSON.parse(localStorage.getItem(keys[i]));
+  }
+
+  archive.forEach(function (item) {
+    const dataQueue = item[0].librarySection;
+    if (dataQueue === queue) {
+      refs.filmsContainer.insertAdjacentHTML('beforeend', collectionLibraryCardTpl(item));
+    }
   });
 }
