ef6a63a67e1d6d8e4e4dc63d4aad136b8cf0650a
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-08-15 17:43:22 +0300

    старт

---
+++ src/js/pagination/start-pagination.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Demo from './pagination';
+console.log(Demo);
+
+pag1();
+export function pag1() {
+  //   ReactDOM.render(<Demo />, document.querySelector('#root_header'));
+  ReactDOM.render(<Demo />, document.querySelector('#root'));
+}
