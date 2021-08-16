e3e995ccc57acaa0a94a0ba9b524e30466788ce8
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-08-16 21:41:26 +0300

    Первый готов

--- src/js/pagination/paginationPopul.js
+++ src/js/pagination/paginationPopul.js
@@ -4,54 +4,37 @@ import { Link } from 'react-router-dom';
 import Pagination from '@material-ui/lab/Pagination';
 import PaginationItem from '@material-ui/lab/PaginationItem';
 
-// import { refs } from '../refs';
+import { refs } from '../refs';
 import { fetchPopularCollection } from '../fetch-popular.js';
 import { renderPopularCollection } from '../page-popular.js';
-// import { dataCollection } from '../page-popular.js';
 
-// let totalPages = '';
-// console.log(totalPages);
-
-// export function dataPages() {
-//   const totalPages = refs.filmsContainer.getAttribute('dataPage');
-//   console.log(totalPages);
-//   return totalPages;
-// }
-// console.log(totalPages);
-
-const totalPages = 1000;
+// const totalPages = 1000;
 
 export default function PaginationLink() {
-  //   const fff = setTimeout(dataPages(), 5000);
-  //   console.log(fff);
   return (
     <MemoryRouter initialEntries={['/inbox']} initialIndex={0}>
       <Route>
         {({ location }) => {
           const query = new URLSearchParams(location.search);
           const page = parseInt(query.get('page') || '1', 10);
-          //  const totalPages = function () {
-          //    const Pages = refs.filmsContainer.getAttribute('dataPage');
-          //    console.log(Pages);
-          //    return totalPages;
-          //  };
-          //  console.log(totalPages);
-          //  dataCollection(page);
 
           fetchPopularCollection(page).then(renderPopularCollection);
 
-          //  function ddd() {
-          //    const totalPages = refs.filmsContainer.getAttribute('dataPage');
-          //    console.log(totalPages);
-          //    return totalPages;
-          //  }
-          //  let dsdd = ddd();
-          //  console.log(dsdd);
+          function dataPages() {
+            const totalPages = refs.filmsContainer.getAttribute('dataPage');
+            // console.log(totalPages);
+            return totalPages;
+          }
+          let pagesStr = dataPages();
+          //  console.log(typeof pagesStr);
+
+          let pagesNumbar = Number(pagesStr);
+          //  console.log(typeof pagesNumbar);
 
           return (
             <Pagination
               page={page}
-              count={totalPages}
+              count={pagesNumbar}
               defaultPage={6}
               siblingCount={2}
               boundaryCount={1}
