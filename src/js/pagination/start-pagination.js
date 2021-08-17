f22456f3948f45fcc02e82c0afc918286376eb27
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-08-17 20:40:07 +0300

    11

--- src/js/pagination/start-pagination.js
+++ src/js/pagination/start-pagination.js
@@ -5,11 +5,11 @@ import PaginPopal from './paginationPopul';
 import { refs } from '../refs.js';
 
 export function pagination() {
-  //   ReactDOM.render(<PaginPopal />, document.querySelector('#root_header'));
+  ReactDOM.render(<PaginPopal />, document.querySelector('#root_header'));
   ReactDOM.render(<PaginPopal />, document.querySelector('#root_futer'));
 }
 
-copyMarkupPagin();
+// copyMarkupPagin();
 
 function copyMarkupPagin() {
   console.log(refs.pagination);
