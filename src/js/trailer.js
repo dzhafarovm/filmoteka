67332bbae3b8e30dc6aa85c28daf46c08ae64ded
Maksym Dzhafarov <maksim8797@gmail.com>
2021-08-18 13:09:58 +0300

    update trailer

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -43,7 +43,7 @@ function renderTrailer(data) {
   });
 
   const trailer = basicLightbox.create(`
-  <iframe width="450" height="320" src='https://www.youtube.com/embed/${key}'frameborder="0" allowfullscreen class="trailer_video"></iframe>
+  <iframe src='https://www.youtube.com/embed/${key}'frameborder="0" allowfullscreen class="trailer_video"></iframe>
 `);
 
   const trailerbtn = document.querySelector('.card__btn-trailer');
