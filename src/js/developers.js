68452b290e57f5eb7301d77aaf2932a53a831acb
Olena Avdiunina <elena.avdiunina@gmail.com>
2021-08-19 00:29:15 +0300

    team mod

--- src/js/developers.js
+++ src/js/developers.js
@@ -1,1 +1,47 @@
 import { refs } from './refs.js';
+import developers from './dev-data';
+import personalTpl from '../hbs/sample-4.hbs';
+import '@fortawesome/fontawesome-free/js/brands';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+
+const developersBoard = document.querySelector('.js-dev-set');
+developersBoard.innerHTML = personalTpl(developers);
+
+
+refs.openModalBtn.addEventListener('click', onOpenModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
+
+function onOpenModal() {
+  window.addEventListener('keydown', onEscKeyPress);
+  refs.backdrop.classList.remove('is-hidden');
+  refs.body.classList.add('overhidden');
+//   refs.backdrop.classList.add('show-modal');
+}
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onEscKeyPress);
+  refs.backdrop.classList.remove('show-modal');
+  refs.backdrop.classList.add('is-hidden');
+  refs.body.classList.remove('overhidden');
+}
+  
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      onCloseModal();
+    }
+}
+  
+  function onEscKeyPress(event) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = event.code === ESC_KEY_CODE;
+  
+    if (isEscKey) {
+
+      onCloseModal();
+
+    }
+}
+
+
+
